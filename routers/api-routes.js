186de6a13bbfcce32bdc0c47dce6fd865b6ff087
julhanlon <jhanlon289@gmail.com>
2020-06-18 22:12:26 -0700

    uuid

--- routers/api-routes.js
+++ routers/api-routes.js
@@ -3,7 +3,8 @@ const router = express.Router();
 const path = require("path");
 const fs = require("fs");
 const util = require("util");
-const uuid = require("uuid");
+const { v4: uuidv4 } = require('uuid');
+var id = uuidv4();
 
 const readFileAsync = util.promisify(fs.readFile);
 const writeFileAsync = util.promisify(fs.writeFile);
@@ -14,34 +15,18 @@ router.get("/api/notes", (req, res) => {
 });
 
 router.post("/api/notes", (req, res) => {
-  console.log("api has been hit");
-  res.json({ message: "this is another button" });
+let id = await readFileAsync("data.json");
+id = JSON.parse(id);
+countData.count++;
+
+await writeFileAsync("data.json", JSON.stringify(id, null, 2));
+res.send(id);
 });
 
 router.delete("/api/notes/:id", (req, res) => {
   console.log("api has been hit");
   res.json({ message: "this is a third example" });
 });
-// endpoint /api/count
-router.get("/api/count", async (req, res) => {
-let countData = await readFileAsync("data.json");
-countData = JSON.parse(countData);
-countData.count++;
 
-await writeFileAsync("data.json", JSON.stringify(countData, null, 2));
-res.send(countData);
-  // fs.readFile("data.json", "utf8", (err, data) => {
-  //   if (err) throw err;
-  //   const currentCount - JSON.parse(data);
-  //   currentCount.count ++;
-
-  //   fs.writeFile("data.json", JSON.stringify(currentCount, null, 2), (err => {
-  //     if (err) throw err;
-  //     res.send(currentCount);
-  //   }))
-
-  // })
- 
-})
 
 module.exports = router;
