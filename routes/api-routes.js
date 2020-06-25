46dca9d78735f12b69085d0cd09472c1dc4b18c0
julhanlon <jhanlon289@gmail.com>
2020-06-25 13:48:17 -0700

    updates

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -2,8 +2,6 @@ const express = require("express");
 const router = express.Router();
 const fs = require("fs");
 const util = require("util");
-const { v4: uuidv4 } = require('uuid');
-var id = uuidv4();
 
 const readFileAsync = util.promisify(fs.readFile);
 const writeFileAsync = util.promisify(fs.writeFile);
