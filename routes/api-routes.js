cc74df1081dcff324cd67f63811c60928fb524f9
julhanlon <jhanlon289@gmail.com>
2020-06-24 19:43:14 -0700

    fixed folder names

---
+++ routes/api-routes.js
@@ -0,0 +1,54 @@
+const express = require("express");
+const router = express.Router();
+const path = require("path");
+const fs = require("fs");
+const util = require("util");
+const { v4: uuidv4 } = require('uuid');
+var id = uuidv4();
+
+const readFileAsync = util.promisify(fs.readFile);
+const writeFileAsync = util.promisify(fs.writeFile);
+
+router.get("/api/notes", (req, res) => {
+ var data = JSON.parse(readFileAsync(".db/db.json", "utf8"));
+ res.json(data);
+});
+
+
+
+
+router.post("/api/notes", (req, res) => {
+  const data = JSON.parse(readFileAsync("./db/todos.json", "utf8"));
+  const todo = req.body;
+req.body = {
+  title: "",
+  text: "",
+};
+var id = uuid.v4();
+req.body.id = id ;
+data.push(todo);
+
+}).then((res) => {
+  writeFileAsync("data.json", JSON.stringify(data, null, 2));
+  res.send(data);
+});
+
+
+
+
+router.delete("/api/notes/:id", (req, res) => {
+  var todoId = req.params.id;
+  var arr = JSON.parse(readFileAsync("./db/db.json", "utf8"));
+
+arr = arr.filter(item => item !== todoId)
+
+console.log(arr)
+
+}).then(() => {
+  writeFileAsync("./db/db.json", JSON.stringify(arr, null, 2));
+});
+
+
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const PORT = 3000;
 //Sets up server for incoming and outgoing datatypes(string, array, json)
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static("client"));
+app.use(express.static("./public/"));
 
 //Sets up client side routes responsible for serving html
 const clientRoutes = require("./routes/client-routes");
@@ -15,8 +15,8 @@ app.use("/", clientRoutes);
 const apiRoutes = require("./routes/api-routes");
 app.use("/", apiRoutes);
 
-const dataRoutes = require("./routes/data-routes");
-app.use(dataRoutes);
+// const dataRoutes = require("./routes/data-routes");
+// app.use(dataRoutes);
 
 //Runs server
 app.listen(PORT, () => console.log(`Listening at http://localhost:${PORT}`));
